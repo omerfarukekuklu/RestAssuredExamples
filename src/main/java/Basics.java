6ae47e4ab64457076f3c1eefad38e49ad8a11eda
omerfarukekuklu <omerfarukekuklu@gmail.com>
2021-10-02 12:17:27 +0300

    AddPlace endpoint tested with rest assured.

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -1,3 +1,15 @@
-public class Basics {
+import fileManager.payloadManager;
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
+public class Basics {
+    public static void main(String[] args) {
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+        given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+                .body(payloadManager.getAddPlaceBody())
+                .when().post("maps/api/place/add/json")
+                .then().log().all().assertThat().statusCode(200).body("scope",equalTo("APP"))
+                .header("Server", "Apache/2.4.18 (Ubuntu)");
+    }
 }
